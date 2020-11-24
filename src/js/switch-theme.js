ea720d50ff2f2b845b90da4fbd5255caa7d3d8af
Joaquín Buendía <jgamer669@gmail.com>
2020-11-24 09:23:47 -0500

    mouseover

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,4 +1,5 @@
 const switchTheme = document.getElementsByClassName('switch__circle')[0]
+const switch__box = switchTheme.parentElement.parentElement
 
 const DarkTheme = {
     '--Toogle' : 'hsl(210, 78%, 56%), hsl(146, 68%, 55%)',
@@ -8,6 +9,7 @@ const DarkTheme = {
     '--BackgroundCard': 'hsl(228, 28%, 20%)',
     '--Small-Text': 'hsl(228, 34%, 66%)',
     '--White-Text': 'hsl(0, 0%, 100%)',
+    '--HoverCard': 'hsl(229, 28%, 29%)'
 }
 
 const LightTheme = {
@@ -17,7 +19,8 @@ const LightTheme = {
     '--BackgroundTop': 'hsl(225, 100%, 98%)',
     '--BackgroundCard': 'hsl(227, 47%, 96%)',
     '--Small-Text': 'hsl(228, 12%, 44%)',
-    '--White-Text': 'hsl(230, 17%, 14%)'
+    '--White-Text': 'hsl(230, 17%, 14%)',
+    '--HoverCard': 'hsl(229, 9%, 75%)'
 }
 
 const changeTheme = (theme)=>{
@@ -34,13 +37,18 @@ let cont = 0
 switchTheme.addEventListener('click',()=>{
     if(cont < 1){
         changeTheme(LightTheme)
-        switchTheme.classList.toggle('active__circle')
-        switchTheme.parentElement.classList.toggle('active__bar')
+        listStyle()
         cont++
     }else if(cont == 1){
         changeTheme(DarkTheme)
-        switchTheme.classList.toggle('active__circle')
-        switchTheme.parentElement.classList.toggle('active__bar')
+        listStyle()
         cont--
     }
 })
+
+const listStyle = ()=>{
+    switchTheme.classList.toggle('active__circle')
+    switchTheme.parentElement.classList.toggle('active__bar')
+    switch__box.classList.toggle('switch__hover')
+    switchTheme.parentElement.classList.toggle('active__bar-bg')
+}
